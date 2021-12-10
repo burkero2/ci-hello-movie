1afa2f4277c0339177e92176cdc9f08a316b8674
burkero2 <ronanburke@codeinstitute.net>
2021-12-10 10:01:00 +0000

    Final Commit: Removed Secret Key

--- movie_list/settings.py
+++ movie_list/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-mvog#hi%%%px9@@kyfybf1p4w!hg&@9sz5eb@p=^8yoja+g(ws')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
